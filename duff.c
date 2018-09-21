52a7969367488cfa976afb876d7061cad03a9b10
Joe Carnuccio <joecar@socal.rr.com>
2018-09-21 12:41:12 -0700

    duff: Add syntactic abbreviation around loop!

--- duff.c
+++ duff.c
@@ -4,15 +4,16 @@ void send(register short *to, register short *from, register count)
 {
 	register n=(count+7)/8;
 	switch(count%8)
-	case 0:	do{	*to = *from++;
-	case 7:		*to = *from++;
-	case 6:		*to = *from++;
-	case 5:		*to = *from++;
-	case 4:		*to = *from++;
-	case 3:		*to = *from++;
-	case 2:		*to = *from++;
-	case 1:		*to = *from++;
-		}while(--n>0);
+	do{
+	case 0:	*to = *from++;
+	case 7:	*to = *from++;
+	case 6:	*to = *from++;
+	case 5:	*to = *from++;
+	case 4:	*to = *from++;
+	case 3:	*to = *from++;
+	case 2:	*to = *from++;
+	case 1:	*to = *from++;
+	}while(--n>0);
 }
 
 int main(int argc, char **argv)
